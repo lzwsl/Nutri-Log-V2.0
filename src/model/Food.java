fdfe4c46a92474d95fa83b1470b02ff774800db0
sherman <#3672H3rmy10v3r96>
2019-07-12 12:38:24 -0700

    STABLE STATE - new food class

--- src/model/Food.java
+++ src/model/Food.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Food {
+
+    private String name;
+    private Integer calories;
+
+    public Food(String name, Integer calories) {
+        this.name = name;
+        this.calories = calories;
+    }
 }
